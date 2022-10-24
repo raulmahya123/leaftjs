60cc04700561dbe9b729a7bfb350c51dba6ad81b
raulmahya123 <raulmahya11@gmail.com>
2022-10-24 12:40:19 +0700

    ROUTER

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  MapContainer,
+  TileLayer
+} from 'react-leaflet'
+import './App.css'
 
-function App() {
+const center = [-6.8995231941771, 107.6210690166196];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <MapContainer
+    center={center}
+    zoom={10}
+    style={{width:'100vw', height:'100vh'}}
+    >  
+    </MapContainer>
+  )
 }
-
-export default App;
