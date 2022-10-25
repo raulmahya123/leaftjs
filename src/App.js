670c62383629db48b54b7fa6ef0ac42577792e30
raulmahya123 <raulmahya11@gmail.com>
2022-10-25 08:06:28 +0700

    beres

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import {
   MapContainer,
-  TileLayer
+  TileLayer,
+  Polygon
 } from 'react-leaflet'
 import './App.css'
+import 'leaflet/dist/leaflet.css';
+import {statesData} from './data'
 
 const center = [-6.8995231941771, 107.6210690166196];
 
@@ -13,7 +16,55 @@ export default function App() {
     center={center}
     zoom={10}
     style={{width:'100vw', height:'100vh'}}
+    className='w-full h-full md:md-cols-2'
     >  
+    <TileLayer
+    url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=AovbylPLNOywA6s1eoNJ"
+    attribution= '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+    />
+   {
+    statesData.features.map((state)=>{
+      const coordinates = state.geometry.coordinates[0].map((item)=> [item[1],item[0]]);
+
+      return (<Polygon
+        pathOptions={{
+          fillColor : "#FD8D3c",
+          fillOpacity : 0.7,
+          weight : 2,
+          opacity : 1,
+          dashArray: 3,
+          color: 'white'
+        }}
+        positions={coordinates}
+        eventHandlers={{
+          mouseover: (e) => {
+            const layer = e.target;
+            layer.setStyle({
+              fillOpacity: 0.7 ,
+              weight: 5,
+              dashArray: "",
+              color: '#666',
+              fillColor: '#D45962'
+            })
+          },
+          mouseout: (e) => {
+            const layer = e.target;
+            layer.setStyle({
+              fillOpacity: 0.7 ,
+              weight: 2,
+              dashArray: "3",
+              color: 'white',
+              fillColor : "#FD8D3c",
+            })
+          },
+          click: (e) => {
+
+          }
+        }}
+        />)
+    })
+   }
+    
     </MapContainer>
   )
 }
